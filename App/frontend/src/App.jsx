de4742969255ce68a92bf61147c1e4e3f2e5aef8
Vernere <Verner.etola@metropolia.fi>
2025-09-03 22:01:40 +0300

    Working shadc frontend

--- App/frontend/src/App.jsx
+++ App/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me!!!</Button>
+      </div>
     </>
   )
 }
