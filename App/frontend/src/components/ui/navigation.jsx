52fd44014838a790143f2dcfd66f6efae66f46df
litioner <litioner@protonmail.com>
2025-09-09 15:56:38 +0300

    Refactor import statements: standardize component imports to PascalCase

--- App/frontend/src/components/ui/navigation.jsx
+++ App/frontend/src/components/ui/navigation.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/Button"
 import { useAuth } from "@/hooks/useAuth";
 import { useNavigate } from "react-router";
 
