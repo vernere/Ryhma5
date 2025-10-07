264350e70b129aa9a15d769f1a1257c748f51e67
Emmi Korhonen <142783862+meemmi@users.noreply.github.com>
2025-10-07 15:10:24 +0300

    Search for tags (#25)
    * Add search for tags. Fix: Selected tags green color changes to grey
    
    * Deleted comments
    
    * Added test for tag search
    
    * Added tests for tags
    
    * Formatted with prettier
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: Vernere <Verner.etola@metropolia.fi>

--- App/frontend/src/components/notes/Sidebar.jsx
+++ App/frontend/src/components/notes/Sidebar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Search, Bell, FilePlus2 } from "lucide-react";
 import { Navigation } from "@/components/ui/navigation";
 import { useNotesStore } from "@/hooks/useNotesStore";
+import { useTagStore } from "@/hooks/useTagStore";
 import { useAuth } from "@/hooks/useAuth";
 import { useInvitationsStore } from "@/hooks/useInvitationsStore";
 import { InvitePopup } from "./InvitePopup";
@@ -21,21 +22,20 @@ const Sidebar = () => {
     fetchFavorites,
     fetchNotes,
     setCurrentUser,
-    deleteNote
+    deleteNote,
   } = useNotesStore();
 
-  const {
-    inbox,
-    getInvites
-  } = useInvitationsStore();
+  const { noteTags, getTags } = useTagStore();
+
+  const { inbox, getInvites } = useInvitationsStore();
 
   const {
     setupNoteCollaboratorSubscription,
     cleanupCollaboratorsSubscription,
     setupInvitesSubscription,
-    cleanupInvitesSubscription
-  }= useRealtimeStore();
-  
+    cleanupInvitesSubscription,
+  } = useRealtimeStore();
+
   const { user } = useAuth();
 
   const [isInvitePopupOpen, setIsInvitePopupOpen] = useState(false);
@@ -46,7 +46,7 @@ const Sidebar = () => {
       fetchFavorites();
       fetchNotes();
       getInvites(user.id);
-
+      getTags();
       setupInvitesSubscription(user.id);
       setupNoteCollaboratorSubscription(user.id);
     }
@@ -57,9 +57,21 @@ const Sidebar = () => {
     };
   }, [user?.id]);
 
-  const filteredNotes = notes.filter((note) =>
-    (note.title || "").toLowerCase().includes((searchQuery || "").toLowerCase())
-  );
+  const filteredNotes = notes.filter((note) => {
+    const query = (searchQuery || "").toLowerCase();
+
+    const inTitle = (note.title || "").toLowerCase().includes(query);
+
+    const tagsForNote = noteTags
+      .filter((t) => t.note_id === note.note_id)
+      .map((t) => t.tags?.name.toLowerCase());
+
+    const inTags = tagsForNote.some((name) =>
+      name.includes(query.toLowerCase()),
+    );
+
+    return inTitle || inTags;
+  });
 
   const handleCreateNote = async () => {
     const n = await createNote();
@@ -89,7 +101,9 @@ const Sidebar = () => {
     <>
       <div className="w-60 bg-white border-r border-gray-200 flex flex-col">
         <div className="p-2 border-b border-gray-200 flex items-center justify-between overflow-ellipsis">
-          <p data-cy="userEmail" className="text-md font-semibold">{user?.email}</p>
+          <p data-cy="userEmail" className="text-md font-semibold">
+            {user?.email}
+          </p>
           <button
             onClick={toggleInvitePopup}
             className="relative p-1 rounded-md hover:bg-gray-100 transition-colors cursor-pointer"
@@ -136,10 +150,7 @@ const Sidebar = () => {
         <Navigation />
       </div>
 
-      <InvitePopup
-        isOpen={isInvitePopupOpen}
-        onClose={toggleInvitePopup}
-      />
+      <InvitePopup isOpen={isInvitePopupOpen} onClose={toggleInvitePopup} />
     </>
   );
 };
