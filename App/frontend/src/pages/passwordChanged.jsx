c2886014989fe1b4303ba615bff203792bc9f63b
emmkor <emmi.korhonen4@metropolia.fi>
2025-11-03 23:38:44 +0200

    Add passwordChanged localization

--- App/frontend/src/pages/passwordChanged.jsx
+++ App/frontend/src/pages/passwordChanged.jsx
@@ -4,10 +4,12 @@ import {Button} from "@/components/ui/button";
 import {Link} from "react-router-dom";
 import {useAuth} from "@/hooks/useAuth.js";
 import {useEffect} from "react";
+import { useTranslation } from "react-i18next";
 
 
 const PasswordChanged = () => {
     const {setPasswordRecovery} = useAuth();
+    const { t } = useTranslation();
 
     useEffect(() => {
         setPasswordRecovery(false);
@@ -18,10 +20,10 @@ const PasswordChanged = () => {
             <Header/>
             <div className="flex-grow flex items-center justify-center">
                 <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md flex-col justify-items-center">
-                    <p className="mb-4 text-lg font-semibold">Your password has been successfully updated!</p>
+                    <p className="mb-4 text-lg font-semibold">{t("password.changed.title")}</p>
                     <div className="flex flex-col">
                         <Button asChild className="bg-secondary text-black shadow-md hover:text-white">
-                            <Link to="/login">Go to Login</Link>
+                            <Link to="/login">{t("password.changed.toLoginButton")}</Link>
                         </Button>
                     </div>
                 </div>
